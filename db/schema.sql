6e257af9f07e69e2865bf68cd9e699ade77d88c1
prince naija <princeachuru@gmail.com>
2020-06-15 23:29:35 -0400

    burger table created

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,10 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 -- so all code following will be used on burgers_db
 USE burgers_db;
+-- create the table burgers 
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(20),
+    devoured BOOLEAN DEFAULT false,
+     PRIMARY KEY (id)
+);

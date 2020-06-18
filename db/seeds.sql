06e49b49a5ae9b2283ca78384c628e927d46fd58
prince naija <princeachuru@gmail.com>
2020-06-17 20:29:26 -0400

    created 3 burger quieries in seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+Insert into burgers (burger_name) values ("Bacon Cheese burger");
+Insert into burgers (burger_name) values ("Chicken Burger");
+Insert into burgers (burger_name) values ("Turkey Burger");

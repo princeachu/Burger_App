1475cdb4cea59786ba49af0fde65dbf06eff0929
prince naija <princeachuru@gmail.com>
2020-06-23 15:16:36 -0400

    create function and all funtion added

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,14 @@
 const orm = require("../config/orm");
+const burger = {
+    all: function (cb) {
+        orm.all("burgers", funtion(res), {
+            cb(res);
+        });
+    },
+    create: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    
+};

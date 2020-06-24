84cfec2308f0ffdd11d7507b6679678c9006de4b
prince naija <princeachuru@gmail.com>
2020-06-24 17:48:40 -0400

    update funtion added to burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,14 +1,18 @@
 const orm = require("../config/orm");
 const burger = {
-    all: function (cb) {
-        orm.all("burgers", funtion(res), {
-            cb(res);
-        });
-    },
-    create: function (cols, vals, cb) {
-        orm.create("burgers", cols, vals, function (res) {
-            cb(res);
-        });
-    },
-    
+  all: function (cb) {
+    orm.all("burgers", function (res) {
+      cb(res);
+    });
+  },
+  create: function (cols, vals, cb) {
+    orm.create("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  update: function (objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function (res) {
+      cb(res);
+    });
+  }
 };

74d016355a95b88af8b96ba1cba54b963e670fac
prince naija <princeachuru@gmail.com>
2020-06-24 20:20:28 -0400

    delete function added to burger.js

--- models/burger.js
+++ models/burger.js
@@ -14,5 +14,10 @@ const burger = {
     orm.update("burgers", objColVals, condition, function (res) {
       cb(res);
     });
+  },
+  delete: function (condition, cb) {
+    orm.delete("burgers", condition, function (res) {
+      cb(res);
+    });
   }
 };
